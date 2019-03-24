d19a6f6abb8f584a75f94f796e1f9bfad9cf18c2
wuxl008 <wuxl008@163.com>
2019-03-24 22:53:51 +0800

    Chapter1Application

---
+++ chapter1/src/main/java/com/learning/chapter1/Chapter1Application.java
@@ -0,0 +1,14 @@
+package com.learning.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+}
