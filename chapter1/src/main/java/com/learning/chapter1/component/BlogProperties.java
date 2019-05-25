d7757985cda67e410999f4b2e069400d4927343d
wuxl008 <wuxl008@163.com>
2019-05-25 11:26:05 +0800

    modify

--- chapter1/src/main/java/com/learning/chapter1/component/BlogProperties.java
+++ chapter1/src/main/java/com/learning/chapter1/component/BlogProperties.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class BlogProperties {
 
     @Value("${name}")
